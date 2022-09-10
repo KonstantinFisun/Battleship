ad35a3b6ef30ce2482dad89ae59a4b50822e30ba
KonstantinFisun <72182894+KonstantinFisun@users.noreply.github.com>
2022-09-10 11:57:10 +0300

    Add print for matrix

--- battleship.pl
+++ battleship.pl
@@ -8,10 +8,25 @@ matrix(Nrows, Ncols, Matrix) :-
     maplist(=(0), Row),
     maplist(=(Row), Matrix).
 
-% Точка запуска игры
-start:-matrix(10,10,Matrix),get_matrix_element(Matrix,1,1,Y),write(Y).
-
 % Получение элемента матрицы
 get_matrix_element(Matrix, RowIndex, ColumnIndex, Element):-
   nth0(RowIndex, Matrix, Row),
   nth0(ColumnIndex, Row, Element).
+  
+% Корректный вывод матрицы
+printMatrix(M) :- printRows(M, 0).
+printRows([], _).
+printRows([H|T], R) :-
+  printRow(H, R, 0),
+  Rpp is R + 1,
+  printRows(T, Rpp).
+printRow([], _, _) :- nl.
+printRow([H|T], R, C) :-
+  write(H),
+  write(" "),
+  Cpp is C + 1,
+  printRow(T, R, Cpp).
+
+% Точка запуска игры
+start:-matrix(10,10,Matrix),printMatrix(Matrix).
+
