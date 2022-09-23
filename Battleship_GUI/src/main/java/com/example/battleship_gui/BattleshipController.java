c562ba379ce2638cc65467b017b5d18f8fb1abdf
KonstantinFisun <72182894+KonstantinFisun@users.noreply.github.com>
2022-09-23 16:52:03 +0300

    Add files via upload

---
+++ Battleship_GUI/src/main/java/com/example/battleship_gui/BattleshipController.java
@@ -0,0 +1,14 @@
+package com.example.battleship_gui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class BattleshipController {
+    @FXML
+    private Label welcomeText;
+
+//    @FXML
+//    protected void onHelloButtonClick() {
+//        welcomeText.setText("Здарова епт!");
+//    }
+}
