4dbbd7b79fa43b6c034c63af1e3a6f40307e963c
Константин Сергеевич Фисун <kostya.ficyn@gmail.com>
2022-10-27 23:00:47 +0300

    delete_useless

--- Battleship_GUI/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.battleship_gui {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires com.dlsc.formsfx;
-
-    opens com.example.battleship_gui to javafx.fxml;
-    exports com.example.battleship_gui;
-}
